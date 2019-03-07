72b69e48e8e2359eff9b019896f30ee2e0e6f22f
Alton Carroll <chaos.five.zero@gmail.com>
2019-03-06 18:39:51 -0600

    correct liri

--- liri.js
+++ liri.js
@@ -5,52 +5,56 @@ var moment = require("moment");
 var request = require("request");
 var axios = require("axios");
 var chalk = require("chalk");
-var util = require("util");
 var log = console.log;
 var Spotify = require("node-spotify-api");
 var command = process.argv[2]; //first cl input 
 var opt = process.argv[3]; //second cl input
 var spotify = new Spotify(keys.spotify);
-var appCount = 0;
-var div = "=====================================================";
-var n = "\n";
+var appRun = false;
+var runCount = 0;
+var gotObject;
 
-function main(command, opt){
     switch (command){
         case "concert-this":
-        showConcert(opt);
-        break;
+            showConcert(opt);
+            break;
 
         case "spotify-this-song":
-        showSong(opt);
-        break;
+            showSong(opt);
+            break;
 
         case "movie-this":
-        showMovie(opt);
-        break;
+            showMovie(opt);
+            break;
 
         case "do-what-it-says":
-        doThis();
-        break;
+            doThis(opt);
+            break;
+        
+        case "":
+            help();
+            break;
 
-        default:  //calls help function
-        help();
-        break;
+        default:
+            help();
+            break;
     }
-
-}
-
 //returned if no command is passed
 function help(){
     log(`
-    ${chalk.yellow(div)}
-    ${chalk.yellow.underline("USAGE:  node liri.js <command> [parameter]")}
-        ${chalk.yellow.underline("Available Commands:")}
-            ${chalk.yellow("concert-this")}
-            ${chalk.yellow("spotify-this-song")}
-            ${chalk.yellow("movie-this")}
-            ${chalk.yellow("do-what-it-says")}
-    ${chalk.yellow(div)}
+    ${chalk.yellow("=====================================================")}
+
+    ${chalk.yellow("USAGE:  node liri.js <COMMAND> [parameter]")}
+
+    ${chalk.yellow("Available Commands:")}
+        ${chalk.yellow("concert-this [parameter]")}
+        ${chalk.yellow("spotify-this-song [parameter]")}
+        ${chalk.yellow("movie-this [parameter]")}
+
+        ${chalk.yellow("ALTERNATE USAGE:  node liri.js ") + chalk.yellow.underline("do-what-it-says") + chalk.yellow(" [NO PARAMETER]")}
+        ${chalk.yellow("Reads command and parameter from random.txt")}
+
+    ${chalk.yellow("=====================================================")}
     `);
 }
 
@@ -59,17 +63,18 @@ function help(){
 function showConcert(){
     if (!opt){
         log(`
-        ${chalk.yellow(div)}
+        ${chalk.yellow("=====================================================")}
            ${chalk.red.underline("You didn't provide an artist")}
-        ${chalk.yellow(div)}
+        ${chalk.yellow("=====================================================")}
         `);
         return;
     }
     else {
-        appCount++;
         var artist = opt.trim();
+        log(artist);
     var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
+        appRun = true;
+        runCount++;
     request(queryUrl, function(error, response, body){
         if (error) return log(error);
         if (!error && response.statusCode === 200){
@@ -77,20 +82,16 @@ function showConcert(){
                 return log("No Results Found");
             };
             var data = JSON.parse(body);
-            for (let i = 0; i < data.length; i++){
-                let num = i+1;
-                let name = data[i].venue.name;
-                let city = data[i].venue.city;
-                let state = data[i].venue.region;
-                let country = data[i].venue.country;
-                let date = moment(data[i].datetime).format("DD MMMM YYYY h:mm");
+            for (let i = 0; i < 3; i++){
+                gotObject = "\n=========================================================" + "\nVenue: " + data[i].venue.name + "\nLocation: " + data[i].venue.city + ", " + data[i].venue.region + " " + data[i].venue.country + "\nDate: " + moment(data[i].datetime).format("DD MMMM YYYY h:mm") + "\n=========================================================\n";
+                appendRandom();
                 log(`
-                ${chalk.blue(div)}
-                    ${chalk.blue("#" + num)}
-                        ${chalk.blue.bold("Venue:      ") + name}
-                        ${chalk.blue.bold("Location:   ") + city + ", " + state + " " + country}
-                        ${chalk.blue.bold("Date:       ") + date}
-                ${chalk.blue(div)}
+            ${chalk.yellow.bold("=====================================================")}
+            ${chalk.yellow.bold("#" + (i+1))}
+                ${chalk.yellow.bold("Venue:      ") + data[i].venue.name}
+                ${chalk.yellow.bold("Location:   ") + data[i].venue.city + ", " + data[i].venue.region + " " + data[i].venue.country}
+                ${chalk.yellow.bold("Date:       ") + moment(data[i].datetime).format("DD MMMM YYYY h:mm")}
+            ${chalk.yellow.bold("=====================================================")}
                 `);
             }
         }
@@ -101,51 +102,50 @@ function showConcert(){
 //get the artist, song name, preview link and album song is from
 //default return is thw sign by ace of base
 
-function showSong(opt){
+function showSong(song){
     if (!opt){
         song = "The Sign Ace of Bass";
     }
     else {
         song = opt.trim();
-        appCount++;
+        appRun = true;
+        runCount++;
     }
         spotify.search({
             type: 'track',
             query: song,
             limit: 5
         }).then(function(response){
-            let obj = response.tracks.items;
-            let item = 0;
+            var obj = response.tracks.items;
+            var item = 0;
             for (var i = 0; i < obj.length; i++){
                 item++;
-                let a = JSON.stringify(obj[i].album.artists);
-                let a_obj = JSON.parse(a);
-                let artists = a_obj.map(a_obj => a_obj.name);
-                let name = obj[i].name;
-                let album = obj[i].album.name;
-                let prev = obj[i].preview_url;
+                var a = JSON.stringify(obj[i].album.artists);
+                var a_obj = JSON.parse(a);
+                var artists = a_obj.map(a_obj => a_obj.name);
+                appendRandom();
+                gotObject = "\n=========================================================" + "\nArtist: " + artists + "\nSong Name: " + obj[i].name + "\nAlbum: " + obj[i].album.name + "\nPreview: " + obj[i].preview_url + "\n=========================================================\n";
                 log(`
                 ${chalk.yellow.bold("Song: " + item)}
-                ${chalk.yellow(div)}
+                ${chalk.yellow("=========================================================")}
 
                     ${chalk.yellow.bold("Artist(s):   ") + chalk.whiteBright(artists)}
-                    ${chalk.yellow.bold("Song Name:   ") + chalk.whiteBright(name)}
-                    ${chalk.yellow.bold("Album:       ") + chalk.whiteBright(album)}
-                    ${chalk.yellow.bold("Preview:     ") + chalk.underline.whiteBright(prev)}
+                    ${chalk.yellow.bold("Song Name:   ") + chalk.whiteBright(obj[i].name)}
+                    ${chalk.yellow.bold("Album:       ") + chalk.whiteBright(obj[i].album.name)}
+                    ${chalk.yellow.bold("Preview:     ") + chalk.whiteBright(obj[i].preview_url)}
                 
-                ${chalk.yellow(div)}
+                ${chalk.yellow("=========================================================")}
                 `);
             }
         }).catch(function(err){
             log(err);
         });
-        
     }
 //use command movie-this using movie name
 //get the title, year, IMDB rating, rotton tomatoes rating, language, plot, and actors
 //default is mr.nobody
 
-function showMovie(opt) {
+function showMovie() {
     var firstLine;
     var secondLine;
     if (!opt) {
@@ -155,75 +155,65 @@ function showMovie(opt) {
             ${chalk.yellow("May I suggest Mr. Nobody")}
         `);
     }
-    appCount++;
+    appRun = true;
+    runCount++;
     axios.get("http://www.omdbapi.com/?t=" + opt.trim() + "&plot=short&apikey=trilogy")
         .then(function (response) {
-            let thisResult = response.data;
-            let mov1 = response.data.Title;
-            let mov2 = response.data.Year;
-            let mov3 = response.data.imdbRating;
-            let mov4 = response.data.rottenTomatos;
-            let mov5 = response.data.Country;
-            let mov6 = response.data.Language;
-            let mov7 = response.data.Plot;
-            let mov8 = response.data.Actors;
-
+            gotObject = "\n=========================================================" + "\nTitle: " + response.data.Title + "\nYear: " + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating + "\nRotten Tomatos Rating: " + response.data.rottenTomatos + "\nCountry Produced: " + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors + "\n=========================================================\n";
+            appendRandom();
             log(`
-            ${chalk.yellow(div)}
-            ${chalk.yellow("Title: ") + mov1}
-            ${chalk.yellow("Released: ") + mov2}
-            ${chalk.yellow("IMDB Rating: ") + mov3}
-            ${chalk.yellow("Rotten Tomatos Rating: ") + mov4}
-            ${chalk.yellow("Country Produced: ") + mov5}
-            ${chalk.yellow("Language: ") + mov6}
-            ${chalk.yellow("Plot: ") + mov7}
-            ${chalk.yellow("Actors: ") + mov8}
-            ${chalk.yellow(div)}
+            ${chalk.yellow("=========================================================")}
+            ${chalk.yellow("Title: ") + response.data.Title}
+            ${chalk.yellow("Released: ") + response.data.Year}
+            ${chalk.yellow("IMDB Rating: ") + response.data.imdbRating}
+            ${chalk.yellow("Rotten Tomatos Rating: ") + response.data.rottenTomatos}
+            ${chalk.yellow("Country Produced: ") + response.data.Country}
+            ${chalk.yellow("Language: ") + response.data.Language}
+            ${chalk.yellow("Plot: ") + response.data.Plot}
+            ${chalk.yellow("Actors: ") + response.data.Actors}
+            ${chalk.yellow("=========================================================")}
             `);
-            fs.writeFile("./random" + appCount + ".txt", util.inspect(thisResult), function(err){
-                if (err) {
-                    return log(err);
-                }
-                    log("File was saved");
-            });
         })
         .catch(function (error) {
             log(error);
         });
-        
 }
 
 //use command do-what-it-says
 //read the text from random.txt and return a band, song, or movie the same as above
-function doThis(opt) {
+function doThis() {
     fs.readFile("random.txt", "utf8", function (error, data) {
-        appCount++;
-        let text = data.split(",");
-        command = text[0].trim();
-        opt = text[1].trim();
-        if (error) {
-            log(error);
-            log("Nothing was Found that I could do");
-        }
-        else if (command === "concert-this"){
-            main(command, opt);
-            // log("showConcert");
-            // log(command);
-            // log(opt);
-        }
-        else if (command === "spotify-this-song"){
-            main(command, opt);
-            // log("showSong");
-            // log(command);
-            // log(opt);
-        }
-        else if (command === "movie-this"){
-            main(command, opt);
-            // log("showMovie");
-            // log(command);
-            // log(opt);
+        appRun = true;
+        runCount++;
+        var text = data.split(",");
+        command = text[0];
+        opt = text[1];
+        log(command);
+        log(opt);
+        switch(command) {
+            case "concert-this":
+            showConcert(opt);
+            break;
+
+            case "spotify-this-song":
+            showSong(opt);
+            break;
+
+            case "movie-this":
+            showMovie(opt);
+            break;
+            
+            default: //calls help function
+            help();
+            break;
         }
     });
 }
 
-main (command, opt);
+//output a txt file with the returned data
+//check if a file exists...and name it in sequence
+function appendRandom() {
+    fs.appendFile("./random.txt", gotObject, function (err) {
+        if (err) throw err;
+    });
+}
